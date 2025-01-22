8a40d45cd21bb2adba4e8f908dce564a98d07d43
ExSYaseru21_tv <98365635+ExerSolution@users.noreply.github.com>
2025-01-22 11:08:31 +0800

    commit

--- utils/supabase/client.ts
+++
@@ -1,8 +0,0 @@
-import { createBrowserClient } from '@supabase/ssr'
-const PROJECT_URL = process.env.PROJECT_URL || "";
-const ANON_PUBLIC = process.env.ANON_PUBLIC || "";
-export function createClient() {
-  return createBrowserClient(
-    PROJECT_URL,ANON_PUBLIC
-  )
-}
